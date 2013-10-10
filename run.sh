da3a4a21aeda3b1573ffbdeb19e15da395cb1389
Andrew Brampton <me@bramp.net>
2013-10-10 18:17:50 -0400

    Minor changes to run.sh

--- run.sh
+++ run.sh
@@ -2,20 +2,21 @@
 set -o xtrace
 
 export PHANTOM="$HOME/vendor/phantomjs-1.9.2-linux-x86_64/bin/phantomjs"
-export CMD="timeout -k 1s 30s $PHANTOM --config=config.json netsniff.js"
+export CMD="timeout -k 1s 45s $PHANTOM --config=config.json netsniff.js"
 
 
 function fetch {
 	echo "Fetching $0"
+	KEY=$(echo $0 | cut -c 1-3)
 
-	if [ ! -f desktop/$0.html ]; then
+	if [ ! -f desktop/$KEY/$0.html ]; then
         	$CMD $0;
 	fi
 
-	if [ ! -f mobile/$0.html ]; then
+	if [ ! -f mobile/$KEY/$0.html ]; then
         	$CMD $0 --mobile;
 	fi
 }
 export -f fetch
 
-awk -F, '{print $2}' top-1m.csv | parallel -j8 --progress fetch :::: -
+awk -F, '{print $2}' top-1m.csv | parallel -j8 --progress fetch :::: - | tee log
